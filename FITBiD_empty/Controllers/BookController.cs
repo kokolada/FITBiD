a40adebd55f77af01f7124718f5cdd57c3ffe29d
IB120133 <IB120133@d21d1406-d953-9443-9845-05699c70ec72>
2014-12-16 16:11:56 +0000

    NAPRAVICU POSEBAN PROJEKAT ZA DESKTOP VERZIJU; LAKSE PUNO TAKO

--- FITBiD_empty/Controllers/BookController.cs
+++ FITBiD_empty/Controllers/BookController.cs
@@ -5,68 +5,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Faker;
 
 namespace FITBiD_empty.Controllers
 {
-    public class BookController : DAController
-    {
-        MojContext ctx = new MojContext();
-        
-        public ActionResult Index()
-        {
-            List<Knjiga> Model = ctx.Knjiga.ToList();
-            return View(Model);
-        }
+	public class BookController : DAController
+	{
+		MojContext ctx = new MojContext();
+		
+		public ActionResult Index()
+		{
+			List<Knjiga> Model = ctx.Knjiga.ToList();
+			return View(Model);
+		}
 
-        public ActionResult Create()
-        {            
-            return View();
-        }        
+		public ActionResult Create()
+		{            
+			return View();
+		}        
 
-        public ActionResult Edit(int bookId)
-        {
-            Knjiga Model = ctx.Knjiga.Find(bookId);            
-            return View(Model);
-        }
-        
-        public ActionResult Save(Knjiga k)
-        {
-            Knjiga knjiga;
-            if (k.Id == 0)
-            {
-                knjiga = new Knjiga();
-                ctx.Knjiga.Add(knjiga);
-            }
-            else
-                knjiga = ctx.Knjiga.Find(k.Id);
-           
-            knjiga.GodinaObjavljivanja = k.GodinaObjavljivanja;
-            knjiga.Naziv = k.Naziv;
-            knjiga.Autor = k.Autor;
-            knjiga.Barcode = k.Barcode;
-            knjiga.OznakaStalaze = k.OznakaStalaze;
-            knjiga.Cijena = k.Cijena;
-            knjiga.ZaProdaju = k.ZaProdaju;
-            knjiga.KnjigaKategorijas = k.KnjigaKategorijas;
-            
-            ctx.SaveChanges();
+		public ActionResult Edit(int bookId)
+		{
+			Knjiga Model = ctx.Knjiga.Find(bookId);            
+			return View(Model);
+		}
+		
+		public ActionResult Save(Knjiga k)
+		{
+			Knjiga knjiga;
+			if (k.Id == 0)
+			{
+				knjiga = new Knjiga();
+				ctx.Knjiga.Add(knjiga);
+			}
+			else
+				knjiga = ctx.Knjiga.Find(k.Id);
+		   
+			knjiga.GodinaObjavljivanja = k.GodinaObjavljivanja;
+			knjiga.Naziv = k.Naziv;
+			knjiga.Autor = k.Autor;
+			knjiga.Barcode = k.Barcode;
+			knjiga.OznakaStalaze = k.OznakaStalaze;
+			knjiga.Cijena = k.Cijena;
+			knjiga.ZaProdaju = k.ZaProdaju;
+			knjiga.KnjigaKategorijas = k.KnjigaKategorijas;
+			
+			ctx.SaveChanges();
 
-            return RedirectToAction("Index");
-        }
+			return RedirectToAction("Index");
+		}
 
-        public ActionResult Delete(int id)
-        {
-            Knjiga k = ctx.Knjiga.Find(id);
-            ctx.Knjiga.Remove(k);
-            ctx.SaveChanges();
+		public ActionResult Delete(int id)
+		{
+			Knjiga k = ctx.Knjiga.Find(id);
+			ctx.Knjiga.Remove(k);
+			ctx.SaveChanges();
 
-            return RedirectToAction("Index");
-        }
-       
-        public ActionResult Details(int id)
-        {
-            Knjiga Model = ctx.Knjiga.Find(id);
-            return View(Model);
-        }
-    }
+			return RedirectToAction("Index");
+		}
+	   
+		public ActionResult Details(int id)
+		{
+			Knjiga Model = ctx.Knjiga.Find(id);
+			return View(Model);
+		}
+	}
 }
