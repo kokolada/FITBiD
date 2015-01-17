98bd440fb14ac11a2f5683d1b795508bcd5fb535
IB120133 <IB120133@d21d1406-d953-9443-9845-05699c70ec72>
2015-01-17 17:45:34 +0000

    login

---
+++ FITBiD_web/FITBiD_empty/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using FITBiD_empty.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FITBiD_empty.Controllers
+{
+	public class LoginController : Controller
+	{
+		// GET: Login
+		public ActionResult Index()
+		{
+
+			ViewBag.Message = "Login stranica";
+
+			return View("Login");
+		}
+
+		[HttpPost]
+		public ActionResult Index(string username, string password)
+		{
+			using (MojContext ctx = new MojContext())
+			{
+				var user = ctx.Radnik.Where(x => x.Username == username).Where(x => x.Password == password).FirstOrDefault();
+				if (user != null)
+				{
+					Session.Add("user", user);
+					return Redirect("/");
+				}
+				else
+				{
+					var student = ctx.Student.Where(x => x.BrojIndeksa == username).Where(x => x.Password == password).FirstOrDefault();
+					if (student != null)
+					{
+						Session.Add("user", student);
+						return Redirect("/Profile/Index?studentId=1");
+					}
+				}
+			}
+			return View("Login");
+		}
+	}
+}
